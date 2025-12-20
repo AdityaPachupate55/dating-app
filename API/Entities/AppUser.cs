abfd553d09694a56bfa1813a14e3d90126b43d53
AdityaPachupate55 <Aditya.Pachupate@cognizant.com>
2025-12-20 10:59:48 +0530

    feat(members): implement Members and Photos entities, repository, and controller; add user seeding functionality

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,10 +7,12 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    public string? ImageUrl { get; set; }
     public required byte[] passwordHash { get; set; }
     public required byte[] passwordSalt { get; set; }
 
-
+    //Nav props
+    public Member Member { get; set; } = null!;
 
 
 }
